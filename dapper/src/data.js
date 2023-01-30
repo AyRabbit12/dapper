d52cfc01e9cd8a1181e1c7de756458afe7700818
Rafael <mendozar1296@gmail.com>
2023-01-29 16:37:14 -0800

    cart

---
+++ dapper/src/data.js
@@ -0,0 +1,30 @@
+import sweater from './img/sweater.jpg';
+import coat from './img/coat2.png';
+import pajamas from './img/pajamas.png';
+
+
+
+
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Sweater',
+        price: 20,
+        image: sweater,
+      },
+      {
+        id: '2',
+        name: 'Pajamas',
+        price: 15,
+        image: pajamas,
+      },
+      {
+        id: '3',
+        name: 'Coat',
+        price: 40,
+        image: coat,
+      },
+    ],
+  };
+  export default data;
